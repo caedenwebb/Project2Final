38aae24e930109ae677fe2ed3f997c7fafd25ec3
Caeden Webb <caedenwebb@unomaha.edu>
2023-05-12 01:51:55 -0500

    functionality for determining point values prepared

--- control.py
+++ control.py
@@ -24,15 +24,15 @@ def start(window: Ui_MainWindow) -> None:
     window.SRButton.clicked.connect(lambda:reset(Gb, window, p1, p2))
 
     #initialize gameboard buttons
-    window.B0Button.clicked.connect(lambda:mark_board(Gb, p1, window, 0, p2))
-    window.B1Button.clicked.connect(lambda:mark_board(Gb, p1, window, 1, p2))
-    window.B2Button.clicked.connect(lambda:mark_board(Gb, p1, window, 2, p2))
-    window.B3Button.clicked.connect(lambda:mark_board(Gb, p1, window, 3, p2))
-    window.B4Button.clicked.connect(lambda:mark_board(Gb, p1, window, 4, p2))
-    window.B5Button.clicked.connect(lambda:mark_board(Gb, p1, window, 5, p2))
-    window.B6Button.clicked.connect(lambda:mark_board(Gb, p1, window, 6, p2))
-    window.B7Button.clicked.connect(lambda:mark_board(Gb, p1, window, 7, p2))
-    window.B8Button.clicked.connect(lambda:mark_board(Gb, p1, window, 8, p2))
+    window.B0Button.clicked.connect(lambda:mark_board(Gb, p1, window, 0, p2, 9))
+    window.B1Button.clicked.connect(lambda:mark_board(Gb, p1, window, 1, p2, 9))
+    window.B2Button.clicked.connect(lambda:mark_board(Gb, p1, window, 2, p2, 9))
+    window.B3Button.clicked.connect(lambda:mark_board(Gb, p1, window, 3, p2, 9))
+    window.B4Button.clicked.connect(lambda:mark_board(Gb, p1, window, 4, p2, 9))
+    window.B5Button.clicked.connect(lambda:mark_board(Gb, p1, window, 5, p2, 9))
+    window.B6Button.clicked.connect(lambda:mark_board(Gb, p1, window, 6, p2, 9))
+    window.B7Button.clicked.connect(lambda:mark_board(Gb, p1, window, 7, p2, 9))
+    window.B8Button.clicked.connect(lambda:mark_board(Gb, p1, window, 8, p2, 9))
     pass
 
 def reset(board: Gameboard, window: Ui_MainWindow, player1, player2) -> None:
@@ -83,7 +83,7 @@ def reset(board: Gameboard, window: Ui_MainWindow, player1, player2) -> None:
 
     pass
 
-def mark_board(Gb: Gameboard, player: Player, window: Ui_MainWindow, pos: int, altplayer: Player) -> None:
+def mark_board(Gb: Gameboard, player: Player, window: Ui_MainWindow, pos: int, altplayer: Player, spaces_open: int) -> None:
     '''
     Function called when any of the buttons constituting the board are pressed
     :param window: MainWindow object
@@ -96,89 +96,108 @@ def mark_board(Gb: Gameboard, player: Player, window: Ui_MainWindow, pos: int, a
         window.B0Button.setText(player.char)
         window.B0Button.setEnabled(False)
         Gb.mark_gameboard(0, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 1):
         window.B1Button.setText(player.char)
         window.B1Button.setEnabled(False)
         Gb.mark_gameboard(1, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 2):
         window.B2Button.setText(player.char)
         window.B2Button.setEnabled(False)
         Gb.mark_gameboard(2, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 3):
         window.B3Button.setText(player.char)
         window.B3Button.setEnabled(False)
         Gb.mark_gameboard(3, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 4):
         window.B4Button.setText(player.char)
         window.B4Button.setEnabled(False)
         Gb.mark_gameboard(4, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 5):
         window.B5Button.setText(player.char)
         window.B5Button.setEnabled(False)
         Gb.mark_gameboard(5, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 6):
         window.B6Button.setText(player.char)
         window.B6Button.setEnabled(False)
         Gb.mark_gameboard(6, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 7):
         window.B7Button.setText(player.char)
         window.B7Button.setEnabled(False)
         Gb.mark_gameboard(7, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
     if (pos == 8):
         window.B8Button.setText(player.char)
         window.B8Button.setEnabled(False)
         Gb.mark_gameboard(8, player.char)
+        #spaces_open = spaces_open - 1
+        #print(f'{pos} spot clicked;{spaces_open} spaces remaining')
 
     #check for victory or tie
     if (Gb.check_victory() == 'X'):
         enable_buttons(window)
         clear_buttons(window)
         round_incomplete = True
+        #spaces_open = 9
     elif (Gb.check_victory() == 'O'):
         enable_buttons(window)
         clear_buttons(window)
         round_incomplete = False
-    elif (Gb.check_victory() == 'NO VICTORY'):
-        pass
-    elif (Gb.check_victory() == 'TIE'):
+        #spaces_open = 9
+    elif (spaces_open == 0):
         enable_buttons(window)
         clear_buttons(window)
-        round_incomplete = False
+        round_incomplete = True
+        #spaces_open = 9
     else:
         pass
 
     #switch players
     if (round_incomplete == True):
-        window.B0Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 0, player))
-        window.B1Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 1, player))
-        window.B2Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 2, player))
-        window.B3Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 3, player))
-        window.B4Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 4, player))
-        window.B5Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 5, player))
-        window.B6Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 6, player))
-        window.B7Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 7, player))
-        window.B8Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 8, player))
+        window.B0Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 0, player, spaces_open))
+        window.B1Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 1, player, spaces_open))
+        window.B2Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 2, player, spaces_open))
+        window.B3Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 3, player, spaces_open))
+        window.B4Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 4, player, spaces_open))
+        window.B5Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 5, player, spaces_open))
+        window.B6Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 6, player, spaces_open))
+        window.B7Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 7, player, spaces_open))
+        window.B8Button.clicked.connect(lambda:mark_board(Gb, altplayer, window, 8, player, spaces_open))
     else:
         if (player.char == 'X'):
-            window.B0Button.clicked.connect(lambda: mark_board(Gb, player, window, 0, altplayer))
-            window.B1Button.clicked.connect(lambda: mark_board(Gb, player, window, 1, altplayer))
-            window.B2Button.clicked.connect(lambda: mark_board(Gb, player, window, 2, altplayer))
-            window.B3Button.clicked.connect(lambda: mark_board(Gb, player, window, 3, altplayer))
-            window.B4Button.clicked.connect(lambda: mark_board(Gb, player, window, 4, altplayer))
-            window.B5Button.clicked.connect(lambda: mark_board(Gb, player, window, 5, altplayer))
-            window.B6Button.clicked.connect(lambda: mark_board(Gb, player, window, 6, altplayer))
-            window.B7Button.clicked.connect(lambda: mark_board(Gb, player, window, 7, altplayer))
-            window.B8Button.clicked.connect(lambda: mark_board(Gb, player, window, 8, altplayer))
+            window.B0Button.clicked.connect(lambda: mark_board(Gb, player, window, 0, altplayer, 9))
+            window.B1Button.clicked.connect(lambda: mark_board(Gb, player, window, 1, altplayer, 9))
+            window.B2Button.clicked.connect(lambda: mark_board(Gb, player, window, 2, altplayer, 9))
+            window.B3Button.clicked.connect(lambda: mark_board(Gb, player, window, 3, altplayer, 9))
+            window.B4Button.clicked.connect(lambda: mark_board(Gb, player, window, 4, altplayer, 9))
+            window.B5Button.clicked.connect(lambda: mark_board(Gb, player, window, 5, altplayer, 9))
+            window.B6Button.clicked.connect(lambda: mark_board(Gb, player, window, 6, altplayer, 9))
+            window.B7Button.clicked.connect(lambda: mark_board(Gb, player, window, 7, altplayer, 9))
+            window.B8Button.clicked.connect(lambda: mark_board(Gb, player, window, 8, altplayer, 9))
         else:
-            window.B0Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 0, player))
-            window.B1Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 1, player))
-            window.B2Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 2, player))
-            window.B3Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 3, player))
-            window.B4Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 4, player))
-            window.B5Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 5, player))
-            window.B6Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 6, player))
-            window.B7Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 7, player))
-            window.B8Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 8, player))
+            window.B0Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 0, player, 9))
+            window.B1Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 1, player, 9))
+            window.B2Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 2, player, 9))
+            window.B3Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 3, player, 9))
+            window.B4Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 4, player, 9))
+            window.B5Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 5, player, 9))
+            window.B6Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 6, player, 9))
+            window.B7Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 7, player, 9))
+            window.B8Button.clicked.connect(lambda: mark_board(Gb, altplayer, window, 8, player, 9))
 
 def enable_buttons(window):
     window.B0Button.setEnabled(True)
