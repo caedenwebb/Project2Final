f22106c925233a5626eeae2890d35c5b1f8844a1
Caeden Webb <caedenwebb@unomaha.edu>
2023-05-11 22:38:28 -0500

    main and control files created; start(), reset(), and mark_board() functions created

---
+++ control.py
@@ -0,0 +1,29 @@
+from player import Player
+from gameboard import Gameboard
+from main import Ui_MainWindow
+
+def start(window: Ui_MainWindow) -> None:
+    '''
+    Function called when start button is pressed
+    :param window: MainWindow object
+    :return: None
+    '''
+
+    pass
+
+def reset(window: Ui_MainWindow) -> None:
+    '''
+    Function called when the reset button is pressed
+    :param window: MainWindow object
+    :return: None
+    '''
+    pass
+
+def mark_board(player: Player, window: Ui_MainWindow, pos: int) -> None:
+    '''
+    Function called when any of the buttons constituting the board are pressed
+    :param window: MainWindow object
+    :param pos: number representing which position on the board was clicked
+    :return: None
+    '''
+    pass
